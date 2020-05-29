aa8a1a2b74476c58e0cd27bd03635a4bca51900d
nihnvm <58342722+nihnvm@users.noreply.github.com>
2020-05-29 19:41:32 +0200

    Add files via upload

---
+++ badCodeThomas.js
@@ -0,0 +1,55 @@
+import React , {useState, useContext}  from 'react';
+import TaskContext from '../../context/task/taskContext';
+
+
+const TaskCard = ({title, text, date, status, click, buttonName, children, id, arg}) => {
+    const taskContext = useContext(TaskContext);
+    const {updateTaskStatus} = taskContext;
+
+    const onChange = e => setBody({ ...body, [e.target.name]: e.target.value });
+
+    const [ body, setBody] = useState({
+        newStatus: ''
+    });
+
+    const {newStatus} = body;
+
+    const onSubmitHandler = (e) => {
+       e.preventDefault();
+        updateTaskStatus(id,body.newStatus);
+        console.log(id,body.newStatus);
+    }
+
+    const clickHandler = () => {
+        if (click)
+            if(arg) 
+                click(arg);
+            else click();
+    }
+
+    return (
+        <div className="card bg-light half rounded">
+        <h3>{title}</h3>
+        <p>{text}</p>
+        <h5>{date}</h5>
+        <h5>{status}</h5>
+        <form onSubmit={onSubmitHandler}>
+                <input 
+                    type="text" 
+                    name="newStatus"
+                    onChange={onChange}
+                    value={body.newStatus}
+                    placeholder="Ny Status?"
+                />
+                <input type="submit" value="Oppdater Status" className="btn btn-dark btn-sm"
+                />
+            </form>
+        <button onClick={clickHandler} className="btn btn-dark btn-sm">{buttonName}</button>
+        {children}
+       
+        </div>
+        
+    )
+}
+
+export default TaskCard;
